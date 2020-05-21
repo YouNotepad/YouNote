3c0ad33b07cb93831431c83a12f0a1a0dc28cee9
ParKjHun <wjdgns7660@gmail.com>
2020-05-21 21:48:40 +0900

    rich text editor added

---
+++ Prototype/src/ckeditor/build-config.js
@@ -0,0 +1,83 @@
+﻿/**
+ * @license Copyright (c) 2003-2020, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.md or https://ckeditor.com/license
+ */
+
+/**
+ * This file was added automatically by CKEditor builder.
+ * You may re-use it at any time to build CKEditor again.
+ *
+ * If you would like to build CKEditor online again
+ * (for example to upgrade), visit one the following links:
+ *
+ * (1) https://ckeditor.com/cke4/builder
+ *     Visit online builder to build CKEditor from scratch.
+ *
+ * (2) https://ckeditor.com/cke4/builder/3a903897f2831daf9e76375c65ac1db9
+ *     Visit online builder to build CKEditor, starting with the same setup as before.
+ *
+ * (3) https://ckeditor.com/cke4/builder/download/3a903897f2831daf9e76375c65ac1db9
+ *     Straight download link to the latest version of CKEditor (Optimized) with the same setup as before.
+ *
+ * NOTE:
+ *    This file is not used by CKEditor, you may remove it.
+ *    Changing this file will not change your CKEditor configuration.
+ */
+
+var CKBUILDER_CONFIG = {
+	skin: 'moono-lisa',
+	preset: 'basic',
+	ignore: [
+		'.DS_Store',
+		'.bender',
+		'.editorconfig',
+		'.gitattributes',
+		'.gitignore',
+		'.idea',
+		'.jscsrc',
+		'.jshintignore',
+		'.jshintrc',
+		'.mailmap',
+		'.npm',
+		'.travis.yml',
+		'bender-err.log',
+		'bender-out.log',
+		'bender.ci.js',
+		'bender.js',
+		'dev',
+		'gruntfile.js',
+		'less',
+		'node_modules',
+		'package.json',
+		'tests'
+	],
+	plugins : {
+		'about' : 1,
+		'allowsave' : 1,
+		'autoplaceholder' : 1,
+		'basicstyles' : 1,
+		'btbutton' : 1,
+		'btquicktable' : 1,
+		'clipboard' : 1,
+		'codemirror' : 1,
+		'colorbutton' : 1,
+		'enterkey' : 1,
+		'entities' : 1,
+		'floatingspace' : 1,
+		'font' : 1,
+		'indentblock' : 1,
+		'indentlist' : 1,
+		'link' : 1,
+		'list' : 1,
+		'maximize' : 1,
+		'pbckcode' : 1,
+		'save' : 1,
+		'save-to-pdf' : 1,
+		'toolbar' : 1,
+		'undo' : 1,
+		'wysiwygarea' : 1
+	},
+	languages : {
+		'en' : 1
+	}
+};
