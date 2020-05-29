1cbd26fe1515cc88260502f7a01cd76c933b183c
HyunSoo Kim <khsoo9312@gmail.com>
2020-05-29 17:57:56 +0900

    initial timestamp function

--- Prototype/src/js/index.js
+++ Prototype/src/js/index.js
@@ -1,4 +1,5 @@
 var embed_video_url = "";
+var videoId = "";
 $(document).ready(function(){
 
     $('#urlBtn').click(function (){
@@ -7,8 +8,8 @@ $(document).ready(function(){
         if(isValidURL){
             $('#urlBtn').prop('href', "main.html");
             //$('#ytplayer').attr('src', embed_video_url);
-            window.localStorage.setItem('initialURL', embed_video_url);
-            
+            window.localStorage.setItem('embedURL', embed_video_url);
+            window.localStorage.setItem('videoId', videoId);
         } else {
             alert("Enter a valid Youtube URL");
         }
@@ -21,6 +22,7 @@ $(document).ready(function(){
             if (match && match[2].length == 11) {
                 // Do anything for being valid
                 // if need to change the url to embed url then use below line
+                videoId = match[2];
                 embed_video_url = 'https://www.youtube.com/embed/' + match[2] + '?autoplay=0';
                 //$('#ytplayerSide').attr('src', 'https://www.youtube.com/embed/' + match[2] + '?autoplay=0');
                 return true;
--- Prototype/src/js/main.js
+++ Prototype/src/js/main.js
@@ -1,61 +1,102 @@
 var embed_video_url = "";
-var videoLists = [];
+var videoId = "";
+var tsList = {};
+var timestamp = 0;
+
+videoId = window.localStorage.getItem('videoId');
+var tag = document.createElement('script');
+
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+var player;
+var videotime = 0;
+
+function onYouTubeIframeAPIReady() {
+    player = new YT.Player('player', {
+        // height: '390',
+        // width: '640',
+        videoId: videoId,
+        events: {
+            'onReady': onPlayerReady,
+        }
+    });
+}
+
+function onPlayerReady(event) {
+    event.target.playVideo();
+    function updateTime() {
+        var oldTime = videotime;
+        if(player && player.getCurrentTime) {
+        videotime = player.getCurrentTime().toFixed(2);
+        document.getElementById("timeurl").innerHTML = videotime;
+        }
+        if(videotime !== oldTime) {
+        onProgress(videotime);
+        }
+    }
+    timeupdater = setInterval(updateTime, 10);
+}
+
+function stopVideo() {
+    player.stopVideo();
+}
+
 
 $(document).ready(function(){
-    var url = window.localStorage.getItem('initialURL');
-    $('#ytplayer').attr('src', url);
-
-    videoLists.push(url);
-    videoLists.push("2nd url");
-    videoLists.push("3rd url");
+    embed_video_url = window.localStorage.getItem('embedURL');
+    // $('#ytplayer').attr('src', url);
     
+    //videoLists.push(url);
+    // videoLists.push("2nd url");
+    // videoLists.push("3rd url");
 
     $('#newVideoURLBtn').click(function(){
         var newUrl  = $('#urlInput').val();
         console.log(newUrl);
         var isValidURL = embed_videoURL_generator(newUrl);
         if(isValidURL){
-            $('#ytplayer').attr('src', embed_video_url);
+            player.loadVideoById({
+                videoId:videoId
+            });
+            
             $('#newVideoURLBtn').prop('href', "main.html");
-            $('#inputNewUrl').remove();
             $('#cancel-btn').click();
+            
+ 
         } else {
             alert("Enter a valid Youtube URL");
-            
         }
     });
 
-    $('#videoLists').click(function(){
-        // var count = 0;
-        // if ($('.modal-videoURLs').is(":empty")){
-        //     videoLists.map(videoURL => {
-        //         $('.modal-videoURLs').append(`
-        //         <div class="d-flex flex-row">
-        //             <div class="p-4 align-self-start">
-        //             <p><i class="fas fa-video"></i><br>URL</p>  
-        //             </div>
-        //             <a href="main.html"><div id="url${count}" class="VideoURL p-4 align-self-end">${videoURL}</div></a>
-        //         </div>
-        //         `);
-        //         count += 1;
-        //     });
-            
-        
-        // }
+    $('#watchNewVideo').click(function(){
+        $('#urlInput').val('');
+    });
 
-        $('.modal-videoURLs').append(`
-            <div id="inputNewUrl" class="d-flex flex-row">
-                <div class="p-4 align-self-start">
-                    <p >new <i class="fas fa-video"></i></p>
-                </div>
-                <div class="VideoURL p-4 align-self-end">
-                
-                    <div class="form-group">
-                        <input id="urlInput" type="text" class="form-control form-control-lg" placeholder="Video URL:">
+    $('#saveTS').click(function(){
+        var tsTitleInput = $('#tsTitle').val();
+        if (tsTitleInput != undefined || tsTitleInput != '') {
+            var len = Object.keys(tsList).length;
+         
+            var key = len.toString().concat(tsTitleInput);
+            tsList[key] = $('#timeurl').val();
+            
+        }
+        var count = 0;
+        if ($('.modal-videoURLs').is(":empty")){
+            videoLists.map(videoURL => {
+                $('.modal-videoURLs').append(`
+                <div class="d-flex flex-row">
+                    <div class="p-4 align-self-start">
+                    <p><i class="fas fa-video"></i><br>URL</p>  
                     </div>
+                    <a href="main.html"><div id="url${count}" class="VideoURL p-4 align-self-end">${videoURL}</div></a>
                 </div>
-            </div>
-        `);
+                `);
+                count += 1;
+            });
+        }
 
     });
 
@@ -67,6 +108,7 @@ $(document).ready(function(){
             if (match && match[2].length == 11) {
                 // Do anything for being valid
                 // if need to change the url to embed url then use below line
+                videoId = match[2];
                 embed_video_url = 'https://www.youtube.com/embed/' + match[2] + '?autoplay=0';
                 //$('#ytplayerSide').attr('src', 'https://www.youtube.com/embed/' + match[2] + '?autoplay=0');
                 return true;
@@ -77,6 +119,55 @@ $(document).ready(function(){
             }
         }
     }
+
+
+    function timestampFunction() {
+        var table= document.getElementById("timestampTable");
+        
+        if ("explanation" != ""){
+            var row = table.insertRow(1);
+            row.classname = "newtimestamp";
+            var c1 = row.insertCell(0);
+            var c2 = row.insertCell(1); 
+            
+            c2.innerHTML = document.getElementById("explanation").value;
+              
+            var player = document.getElementById('player');
+            var time = player.getCurrentTime();
+
+            setTimeout(stopVideo, 6000);
+            if(player && player.getCurrentTime) {
+            videotime = player.getCurrentTime();
+
+            var prettytime= parseInt(videotime);
+            document.getElementById("timeurl").innerHTML = prettytime;
+          }
+        }
+    }
+
+    // function timestampFunction() {
+    //     var table= document.getElementById("timestampTable");
+        
+    //     if ("explanation" != ""){
+    //         var row = table.insertRow(1);
+    //         row.classname = "newtimestamp";
+    //         var c1 = row.insertCell(0);
+    //         var c2 = row.insertCell(1); 
+            
+    //         c2.innerHTML = document.getElementById("explanation").value;
+              
+    //         var player = document.getElementById('player');
+    //         var time = player.getCurrentTime();
+
+    //         setTimeout(stopVideo, 6000);
+    //         if(player && player.getCurrentTime) {
+    //         videotime = player.getCurrentTime();
+
+    //         var prettytime= parseInt(videotime);
+    //         document.getElementById("timeurl").innerHTML = prettytime;
+    //       }
+    //     }
+    // }
 
     
 });
